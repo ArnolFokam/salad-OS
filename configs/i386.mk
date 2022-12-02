2ce0fc32548a98fc9941af16e0e1a08d45a7a1cb
Arnol Fokam <arnolfokam23@gmail.com>
2022-12-02 02:19:06 +0200

    configure linker

--- configs/i386.mk
+++ configs/i386.mk
@@ -4,10 +4,12 @@ AS=as
 
 LDFLAGS = -melf_i386
 
-ASFLAGS += --32 \
-	-I. \
+ASFLAGS = --32
+
+ASFLAGS += I. \
 	-I$(PDIR)/include
 
-CXXFLAGS += -m32 \
-	-I. \
+CXXFLAGS = -m32 -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-exceptions -fno-leading-underscore
+
+CXXFLAGS = -I. \
 	-I$(PDIR)/include

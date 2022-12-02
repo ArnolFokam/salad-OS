243f54ef220b66189e9386175bc4f76fdf3b3055
Arnol Fokam <arnolfokam23@gmail.com>
2022-12-02 02:41:10 +0200

    fix config

--- configs/i386.mk
+++ configs/i386.mk
@@ -2,14 +2,13 @@ PDIR := $(ROOTDIR)/kernel
 
 AS=as
 
-LDFLAGS = -melf_i386
+LDFLAGS += -melf_i386
 
-ASFLAGS = --32
-
-ASFLAGS += I. \
+ASFLAGS += --32 \
+	-I. \
 	-I$(PDIR)/include
-
-CXXFLAGS = -m32 -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-exceptions -fno-leading-underscore
-
-CXXFLAGS = -I. \
+	
+CXXFLAGS += -m32 -fno-use-cxa-atexit -nostdlib \
+	-fno-builtin -fno-rtti -fno-exceptions -fno-leading-underscore \
+	-I. \
 	-I$(PDIR)/include
